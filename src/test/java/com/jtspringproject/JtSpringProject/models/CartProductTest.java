e86e636785672051755b3c943310fcb26281f519
omkar dede <dedeomkar@gmail.com>
2025-03-29 18:24:43 +0530

    added questions

--- src/test/java/com/jtspringproject/JtSpringProject/models/CartProductTest.java
+++ src/test/java/com/jtspringproject/JtSpringProject/models/CartProductTest.java
@@ -19,12 +19,12 @@ class CartProductTest {
 
         String result = "";
 
-        if (n%2 == 0) {
+        if (n % 2 == 0) {
             if (n >= 2 && n <= 5) {
                 result = "Not Weird";
             } else if (n >= 6 && n <= 20) {
                 result = "Weird";
-            } else if (n > 20)  {
+            } else if (n > 20) {
                 result = "Not Weird";
             }
         } else {
@@ -45,8 +45,8 @@ class CartProductTest {
         // 2 x 4 = 8
         // 2 x 5 = 10
         // ...
-        for (int i=1; i<=10; i++) {
-            System.out.println( N + " x " + i + " = " + N * i);
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(N + " x " + i + " = " + N * i);
         }
     }
 
@@ -61,14 +61,14 @@ class CartProductTest {
 
         String smallest = s.substring(0, k);
         String largest = s.substring(0, k);
-        for (int i=0; i+k <= sLen ; i++) {
+        for (int i = 0; i + k <= sLen; i++) {
             String subStr = s.substring(i, i + k);
             if (smallest.compareTo(subStr) > 0)
                 smallest = subStr;
             if (largest.compareTo(subStr) < 0)
                 largest = subStr;
         }
-        System.out.println("The smallest Lexicographically formed substring from welcometojava is : " + smallest +" \n"+ "The largest Lexicographically formed substring from welcometojava is : " + largest);
+        System.out.println("The smallest Lexicographically formed substring from welcometojava is : " + smallest + " \n" + "The largest Lexicographically formed substring from welcometojava is : " + largest);
     }
 
 
@@ -80,13 +80,12 @@ class CartProductTest {
         int n = 5;
         int q = 2;
         int sum = a;
-        for(int i = 0; i < n; i++){
-            sum += (int)Math.pow(2, i) * b;
+        for (int i = 0; i < n; i++) {
+            sum += (int) Math.pow(2, i) * b;
             System.out.print(sum + " ");
         }
         System.out.println();
     }
-
 
 
     @Test
@@ -96,11 +95,11 @@ class CartProductTest {
         String two = "car";
         System.out.println(one.length() + two.length());
         int result = one.compareTo(two);
-        if (result <= 0){
+        if (result <= 0) {
             System.out.println("No");
         } else if (result > 0) {
             System.out.println("Yes");
-        }else {
+        } else {
             System.out.println("hello is equal to java");
         }
         System.out.println(Character.toUpperCase(one.charAt(0)) + one.substring(1) + " " + Character.toUpperCase(two.charAt(0)) + two.substring(1));
@@ -112,6 +111,25 @@ class CartProductTest {
 //        }
 //        return Character.toUpperCase(str.charAt(0)) + str.substring(1);
 //    }
+
+
+    @Test
+    public void theTest6() {
+        // https://www.hackerrank.com/challenges/java-anagrams/problem?isFullScreen=false
+
+    }
+
+    @Test
+    public void theTest7() {
+        // https://www.hackerrank.com/challenges/java-string-reverse/problem?isFullScreen=false
+
+    }
+
+    @Test
+    public void theTest8() {
+        // https://www.hackerrank.com/challenges/java-iterator/problem?isFullScreen=false
+
+    }
 }
 
 
